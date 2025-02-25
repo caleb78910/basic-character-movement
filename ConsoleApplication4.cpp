17bd5fb065c9a05634eccfd84df8296b24bafbb8
caleb78910 <herndoncaleb32@gmail.com>
2025-02-25 14:45:15 -0600

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	bool up = true;
+	bool down = true;
+	bool left = true;
+	bool right = false;
+
+	if (up == true) {
+		cout << "Character moved up" << endl; 
+	}
+	if (down == true) {
+		cout << "Character moved down" << endl;
+	}
+	if (left == true) {
+		cout << "Character moved left" << endl;
+	}
+	if (right == true) {
+		cout << "Character moved right" << endl;
+	}
+
+		
+	
+
+	
+}
